01c015ad935604f1a3935a0385610667f3ef54f8
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-14 16:59:22 +0500

    added getNotById controller

--- backend/src/controllers/notes.controller.js
+++ backend/src/controllers/notes.controller.js
@@ -1,11 +1,28 @@
 import noteModel from "../models/notes.model.js";
 
-export const getNote = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   try {
-    const notes = await noteModel.find({});
+    const notes = await noteModel.find({}).sort({ createdAt: -1 });
     res.status(200).json({
       message: "Notes fetched successfully",
       notes,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getNoteById = async (req, res) => {
+  try {
+    const note = await noteModel.findById(req.params.id);
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    res.status(200).json({
+      message: "Note fetched successfully",
+      note,
     });
   } catch (error) {
     return res.status(500).json({
