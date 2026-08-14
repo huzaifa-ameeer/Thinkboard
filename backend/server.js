37dad6e9065449439f83675922e4f62de1796993
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-14 11:49:12 +0500

    added routes folder

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express"
+import notesRouter from "./routes/notesRoutes.js"
 
 const app = express();
 
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use("/api/notes", notesRouter)
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
