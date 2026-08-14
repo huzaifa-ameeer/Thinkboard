b586d37980cef955cf2403b8dcee2b21c3af82fc
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-14 16:58:49 +0500

    added getNoteById route

--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -1,14 +1,17 @@
 import express from "express";
 import {
   deleteNote,
-  getNote,
+  getAllNotes,
   postNote,
+  getNoteById,
   updateNote,
 } from "../controllers/notes.controller.js";
 
 const router = express.Router();
 
-router.get("/", getNote);
+router.get("/", getAllNotes);
+
+router.get("/:id", getNoteById);
 
 router.post("/", postNote);
 
