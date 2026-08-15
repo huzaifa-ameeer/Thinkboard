946b9542baad08256671ee022a2cedd2d7620c15
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-15 13:43:17 +0500

    config file for upstash added

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+dotenv.config();
+
+const rateLimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "20 s"),
+});
+
+export default rateLimit;
