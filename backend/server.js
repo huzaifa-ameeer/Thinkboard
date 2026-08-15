05d32e3cf6e9cbaa503f7cc0fbdbf466e01f4517
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-15 13:51:35 +0500

    rate limit added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ dotenv.config()
 import express from "express"
 import connectDb from "./src/config/db.js"
 import notesRouter from "./src/routes/notes.routes.js"
+import rateLimiter from "./src/middlewares/rateLimiter.js"
 
 const app = express();
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000
 connectDb()
 
 app.use(express.json())
+app.use(rateLimiter)
 app.use("/api/notes", notesRouter)
 
 app.listen(port, ()=>{
