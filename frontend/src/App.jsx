c5285d10a0bd5a40834115daa4a0b5031454b1a0
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-17 16:16:28 +0500

    added daisyui, react router and some other dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import toast from 'react-hot-toast'
+import {Route, Routes} from "react-router"
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetails from './pages/NoteDetails'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element= {<HomePage/>} />
+        <Route path='/create' element= {<CreatePage/>} />
+        <Route path='/note/:id' element= {<NoteDetails/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/NoteDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteDetails = () => {
+  return (
+    <div>NoteDetails</div>
+  )
+}
+
+export default NoteDetails
