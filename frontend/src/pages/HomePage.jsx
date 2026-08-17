e231a5e3aa4b8b70bd93ddd866bbcab036a80948
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-17 16:44:13 +0500

    feat: added rate limit comp

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
+import RateLimit from '../components/RateLimit'
 
 const HomePage = () => {
+
+  const [isRateLimited, setRateLimited] = useState(false)
+
   return (
     <div className='min-h-screen'>
         <Navbar/>
+        {isRateLimited && <RateLimit/>}
     </div>
   )
 }
